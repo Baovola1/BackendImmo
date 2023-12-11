7f819c4ba37abdd4bb598a2187f27f0bdf638890
Baovola <baovolarav@gmail.com>
2023-12-11 19:21:46 +0100

    create route users test

---
+++ back/models/connection.js
@@ -0,0 +1,12 @@
+
+/*const mongoose = require ('mongoose');
+
+const connectionString = process.env.CONNECTION_STRING
+
+mongoose.connect(connectionString, {connectionTimeoutMS: 2000})
+.then(() => console.log('Database connected'))
+.catch(error => console.error(error));*/
+
+
+
+
