ef01ba4839639121729ffa4253a2d24d3cfa13d2
Baovola <baovolarav@gmail.com>
2023-12-13 15:37:08 +0100

    add google in auth controller

--- back/models/user.js
+++ back/models/user.js
@@ -15,7 +15,11 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
        
-    }
+    },
+    avatar:{
+        type: String,
+        default:"https://static-prod.adweek.com/wp-content/uploads/2020/05/FacebookAvatarsUS-1-652x367.jpg"
+    },
 },{timestamps: true});
 
 const User = mongoose.model('users', userSchema);
