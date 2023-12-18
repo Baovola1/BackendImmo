1fdc1caa771bef4f68f418bee607170f0bb8b51b
Baovola <baovolarav@gmail.com>
2023-12-18 11:28:30 +0100

    add api user delete

--- back/routes/users.js
+++ back/routes/users.js
@@ -3,9 +3,11 @@ const router = express.Router();
 import { test } from "../controllers/user.controller.js";
 import { updatedUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
+import { deleteUser } from "../controllers/user.controller.js";
 
 
 router.get("/test", test);
 router.post("/update/:id", verifyToken, updatedUser);
+router.delete("/delete/:id", verifyToken, deleteUser);
 
 export default router;
