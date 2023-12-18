11da80215e843fc5ab567334285de6a41fac7264
Baovola <baovolarav@gmail.com>
2023-12-18 12:01:24 +0100

    add api logout

--- back/controllers/auth.controller.js
+++ back/controllers/auth.controller.js
@@ -81,3 +81,14 @@ try {
 }
 
 };
+
+//Deconnexion
+export const logout = async (req, res, next)=>{
+    try {
+        res.clearCookie('access_token');
+        res.status(200).json('User has been logged out!');
+    } catch (error) {
+        next(error);
+    }
+
+}
--- back/routes/auth.js
+++ back/routes/auth.js
@@ -3,9 +3,11 @@ const router = express.Router();
 import { signup } from "../controllers/auth.controller.js";
 import { signin } from "../controllers/auth.controller.js";
 import { google } from "../controllers/auth.controller.js";
+import {logout} from "../controllers/auth.controller.js";
 
 router.post('/signup',signup);
 router.post('/signin',signin);
 router.post('/google',google);
+router.get('/logout',logout);
 
 export default router;
