99cb4c60e911213830f04dbb1c11367ec907e43b
Baovola <baovolarav@gmail.com>
2023-12-19 15:10:32 +0100

    create user listings api

--- back/routes/users.js
+++ back/routes/users.js
@@ -3,11 +3,12 @@ const router = express.Router();
 import { test } from "../controllers/user.controller.js";
 import { updatedUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
-import { deleteUser } from "../controllers/user.controller.js";
+import { deleteUser,getUserListings } from "../controllers/user.controller.js";
 
 
 router.get("/test", test);
 router.post("/update/:id", verifyToken, updatedUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id",verifyToken,getUserListings)
 
 export default router;
